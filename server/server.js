039f92c4fd17846decd940f1152408f9ef273965
Djole90 <djole123@gmail.com>
2019-02-24 15:54:15 +0100

    Add POST /todos

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const {mongoose} = require('./db/mongoose')
+const {Todo} = require('./models/todo')
+const {User} = require('./models/user')
+
+const app = express()
+
+app.use(bodyParser.json())
+
+app.post('/todos', (req, res, next) => {
+    console.log(req.body)
+    const todo = new Todo({
+        text: req.body.text
+    })
+    todo.save().then(data => {
+        res.send(data)
+    })
+    .catch(e => res.status(400).send(e))
+})
+
+app.listen(3000, () => {
+    console.log('App started on port', arguments[0])
+})
